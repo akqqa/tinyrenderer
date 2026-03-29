5957593a4456e2341836d5eb9ee2dc3c21e565d5
akqqa <alexk2002@gmail.com>
2026-03-29 14:39:59 +0100

    attempt at scanline rendering

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ constexpr TGAColor yellow  = {  0, 200, 255, 255};
 constexpr int width  = 1000;
 constexpr int height = 1000;
 
-void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+vector<tuple<int,int>> line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    vector<tuple<int,int>> lineCoords;
     // own attempt at steepness resolving
     bool transposed = false;
     if (abs(ay - by) > abs(ax - bx)) { // if more y changes than x
@@ -34,10 +35,39 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
         int y = round( ay + (by-ay)*t );
         if (transposed) {
             framebuffer.set(y, x, color);
+            lineCoords.push_back(make_tuple(y,x));
         } else {
             framebuffer.set(x, y, color);
+            lineCoords.push_back(make_tuple(x,y));
         }
     }
+
+    return lineCoords;
+}
+
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
+    if (ay>by) { swap(ax, bx); swap(ay, by); }
+    if (ay>cy) { swap(ax, cx); swap(ay, cy); }
+    if (by>cy) { swap(bx, cx); swap(by, cy); }
+    vector<tuple<int,int>> abCoords = line(ax, ay, bx, by, framebuffer, color);
+    vector<tuple<int,int>> bcCoords = line(bx, by, cx, cy, framebuffer, color);
+    vector<tuple<int,int>> acCoords = line(ax, ay, cx, cy, framebuffer, color);
+
+    // Figure out triangle filling
+    // WE HAVE OTDER SO A IS HIGHEST AND C IS LOWEST
+    // draw lines between each segment of acCoords and aBCoords, then acCoords and bcCoords
+    // NOT CORRECT AS AC != AB + BC HEIGHT WISE, NEED TO SOMEHOW SCAN DOWN THE Y OF AC AND MATCH WITH CORRECT PLACES IN AB AND BC
+    for (size_t i = 0; i < acCoords.size(); i++) {
+        line(get<0>(acCoords[i]), get<1>(acCoords[i]), get<0>(abCoords[i]), get<1>(abCoords[i]), framebuffer, color);
+    }
+    for (size_t i = 0; i < bcCoords.size(); i++) {
+        line(get<0>(bcCoords[i]), get<1>(bcCoords[i]), get<0>(abCoords[i+acCoords.size()]), get<1>(abCoords[i+acCoords.size()]), framebuffer, color);
+    }
+
+    cout << "ac: " << acCoords.size() << endl;
+    cout << "ab+bc: " << abCoords.size() + bcCoords.size() << endl;
+    cout << "done" << endl;
+
 }
 
 vector<string> splitString(string s, string delimiter) {
@@ -68,7 +98,7 @@ vector<Vertex> readVertices(string filename) {
                 cout << "vertex" << endl;
                 vector<string> lineData = splitString(line, " ");
                 cout << lineData[1] << " " << lineData[2] << " " << lineData[3] << endl;
-                res.push_back(Vertex(stof(lineData[1]),stof(lineData[2]),stof(lineData[3]), width, height));
+                res.push_back(Vertex(stof(lineData[1]),stof(lineData[2]),stof(lineData[3])));
             }
         }
         file.close();
@@ -108,41 +138,38 @@ vector<Face> readFaces(string filename) {
     return res;
 }
 
-void render(vector<Vertex> vertices, vector<Face> faces, TGAImage& framebuffer) {
-    for (Face face : faces) {
-        Vertex v1 = vertices[face.v1];
-        Vertex v2 = vertices[face.v2];
-        Vertex v3 = vertices[face.v3];
-        line(v1.x, v1.y, v2.x, v2.y, framebuffer, red);
-        line(v1.x, v1.y, v3.x, v3.y, framebuffer, red);
-        line(v2.x, v2.y, v3.x, v3.y, framebuffer, red);
-    }
+tuple<int,int> project(Vertex v) {
+    return { (v.x + 1.0) *  width/2, (v.y + 1.0) * height/2 };
+}
 
-    framebuffer.write_tga_file("framebuffer.tga");
+void render(vector<Vertex> vertices, vector<Face> faces, TGAImage& framebuffer) {
+    for (Face f : faces) {
+        auto [ax, ay] = project(vertices[f.v1]);
+        auto [bx, by] = project(vertices[f.v2]);
+        auto [cx, cy] = project(vertices[f.v3]);
+        triangle(ax, ay, bx, by, cx, cy, framebuffer, red);
+    }
+    for (Vertex v : vertices) {
+        auto [ax, ay] = project(v);
+        framebuffer.set(ax, ay, white);
+    }
 }
 
 int main(int argc, char** argv) {
-    TGAImage framebuffer(width, height, TGAImage::RGB);
+    // TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    vector<Vertex> vertices = readVertices("../obj/diablo3_pose/diablo3_pose.obj");
-    vector<Face> faces = readFaces("../obj/diablo3_pose/diablo3_pose.obj");
+    // vector<Vertex> vertices = readVertices("../obj/african_head/african_head.obj");
+    // vector<Face> faces = readFaces("../obj/african_head/african_head.obj");
 
-    render(vertices, faces, framebuffer);
-
-    // int ax =  7, ay =  3;
-    // int bx = 12, by = 37;
-    // int cx = 62, cy = 53;
-
-    // line(ax, ay, bx, by, framebuffer, blue);
-    // line(cx, cy, bx, by, framebuffer, green);
-    // line(cx, cy, ax, ay, framebuffer, yellow);
-    // line(ax, ay, cx, cy, framebuffer, red);
-
-    // framebuffer.set(ax, ay, white);
-    // framebuffer.set(bx, by, white);
-    // framebuffer.set(cx, cy, white);
+    // render(vertices, faces, framebuffer);
 
     // framebuffer.write_tga_file("framebuffer.tga");
-    // return 0;
+
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
+    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
+    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
 }
 
--- vertex.cpp
+++ vertex.cpp
@@ -1,8 +1,8 @@
 class Vertex {
 public:
-    Vertex(float x, float y, float z, int width, int height){
-        this->x = (x + 1.0f) * (width / 2.0f);
-        this->y = (y + 1.0f) * (height / 2.0f);
+    Vertex(float x, float y, float z){
+        this->x = x;
+        this->y = y;
         this->z = z;
     }
 
